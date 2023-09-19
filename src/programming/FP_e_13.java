fe47fc476f1ed2599099b0ba203778f89b05d92a
Abhishek Maity <abhishek.maity@hotmail.com>
2023-09-19 23:46:55 +0530

    added more

---
+++ src/programming/FP_e_13.java
@@ -0,0 +1,26 @@
+package programming;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FP_e_13 {
+
+
+
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+        Function<Integer, Integer> squareNumbers = x -> x * x;
+
+        squareAndPrint(numbers, squareNumbers);
+    }
+
+    private static void squareAndPrint(List<Integer> numbers, Function<Integer, Integer> squareNumbers) {
+        numbers.stream()
+                .map(squareNumbers)
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+    }
+}
