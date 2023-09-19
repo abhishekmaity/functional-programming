bd274c51da30dbb7cef7b36a65cf5f0c048f5aba
Abhishek Maity <abhishekmaity@users.noreply.github.com>
2023-09-20 00:10:40 +0530

    Patch 1 (#3)
    * added more
    
    * updated
    
    * renamed

---
+++ src/programming/FP_e_13.java
@@ -0,0 +1,21 @@
+package programming;
+
+import java.util.List;
+import java.util.function.Function;
+
+public class FP_e_13 {
+
+
+
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+        Function<Integer, Integer> squareNumbers = x -> x * x;
+        
+        numbers.stream()
+                .map(squareNumbers)
+                .toList()
+                .forEach(System.out::println);
+    }
+}
